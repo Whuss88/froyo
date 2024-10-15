c581ebf9e73ab13e5f5d8377a1b65de1a1fdfe44
Whuss88 <williamhussey95@gmail.com>
2024-10-15 11:47:18 -0400

    psuedo code added to script

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // console.log(`hello world`)
 
+// user receives a prompt upon opening the website to enter a list of comma seperated froyo flavors
+// const userinput = prompt ()
+// they type vanilla, vanilla, strawberry, coffee, coffee
+// when they view browser console they observe an object listing of how many of each flavor that have ordered
+// create object containing the user input w/ keys being flavorName & quantity. Values being the actual flavor name and number of time listed 
+// 3 vanilla, 2 coffee, 1 strawberry froyo
+
+// console log showing flavors and number of times it was ordered
