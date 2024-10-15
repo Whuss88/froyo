6bf0a20891a6c1807ae5b86f7de925d28b54bb8e
Whuss88 <williamhussey95@gmail.com>
2024-10-15 12:32:29 -0400

    code added & tested

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // console.log(`hello world`)
 
-// console log showing flavors and number of times it was ordered
 
 // Prompt user to enter a list of froyo flavors separated by commas.
-
-// Capture the user input as a string.
-
-// Split the string by commas to create an array of flavors.
-
+const userInput = prompt(`Enter a list froyo flavors seperated by commas:`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`);
+// Capture the user input as a string. // Split the string by commas to create an array of flavors.
+const flavorArray = userInput.split(`,`);
 // Create an empty object to store the count of each flavor.
-
+const flavorCount = {}
 // Loop through the array of flavors.
-
-// For each flavor in the array:
-
-// Check if the flavor already exists in the object.
-
+for(let i = 0; i < flavorArray.length; i++) {
+  const flavor = flavorArray[i];
+// For each flavor in the array: / Check if the flavor already exists in the object.
+  if (flavorCount[flavor]) {
 // If it does, increment the count by 1.
-
+      flavorCount[flavor]++;
+    } else {
 // If it does not, add the flavor to the object with a count of 1.
-
+      flavorCount[flavor] = 1;
+    }
+}
 // Log the object to the console.
+console.log(flavorCount);
